3e18e06e0be19e22bc11d1c5a0993ee1ea4721c7
Magda98 <magdakochman7@gmail.com>
2023-10-07 10:57:40 +0200

    fire ai

---
+++ FireRecognitionDataset.py
@@ -0,0 +1,84 @@
+from typing import List
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import random_split
+from torchvision import datasets
+import numpy as np
+import pathlib
+
+
+class FireRecognitionDataset:
+    def __init__(self):
+        """
+        Dataloader
+        """
+
+        # workers(processes) in loading data
+        self.num_workers = 0
+
+        # batch size
+        self.batch_size = 5
+
+        # * Horizontal flip - for augmentation
+
+        transform_train = transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+
+        transform_test = transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+
+        # Load data from folders
+        self.data = datasets.ImageFolder(
+            "./fire_dataset/train/", transform=transform_train
+        )
+
+        self.data_test = datasets.ImageFolder(
+            "./fire_dataset/test/", transform=transform_test
+        )
+
+        num_train = len(self.data)
+        self.indices = list(range(num_train))
+
+        num_test = len(self.data_test)
+        self.indices_test = list(range(num_test))
+
+        train_sampler = SubsetRandomSampler(self.indices)
+        test_sampler = SubsetRandomSampler(self.indices_test)
+
+        # dataloaders
+        trainloader = DataLoader(
+            self.data,
+            batch_size=self.batch_size,
+            sampler=train_sampler,
+            num_workers=self.num_workers,
+        )
+        testloader = DataLoader(
+            self.data_test,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            sampler=test_sampler,
+        )
+
+        # classes
+        root = pathlib.Path("./fire_dataset/train/")
+        self.classes = sorted([j.name.split("/")[-1] for j in root.iterdir()])
+        print(self.classes)
+
+        self.trainloader = trainloader
+        self.testloader = testloader
---
+++ cnn.py
@@ -0,0 +1,91 @@
+from typing import List
+from torch.functional import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import math
+
+
+class CnnNet(nn.Module):
+    def __init__(
+        self,
+        input_size,
+        output_size,
+        c_kernels=[7, 5],
+        in_channels=[3, 6],
+        out_channels=[6, 16],
+        p_kernel=[2, 2],
+        p_stride=[2, 2],
+        padding_flag=True,
+        maxpool_freq=2,
+        fc_size=4,
+    ):
+        """
+        CNN class
+        * Architecture: Conv2d [5, 5] -> ReLu -> Conv2d [5, 5] -> ReLu Conv2d [5, 5] -> ReLu -> maxPool2d [2, 2] -> Conv2d [5, 5] -> ReLu -> Conv2d [5, 5] -> ReLu -> maxPool2d [2, 2] -> fc1 -> fc2 -> fc3 -> fc4
+        @ input_size - size of input image
+        @ c_kernels - size of kernel in each conv2d layer
+        @ in_channels, out_channels - define channels in each conv2d layer
+        @ p_kernel - size od kernel in each maxPool2d layer
+        @ p_stride - size of strine in maxPool2d layer
+        """
+        super().__init__()
+        self.cnn = nn.ModuleList()
+        size_out = input_size
+        for i, (k, c_in, c_out) in enumerate(zip(c_kernels, in_channels, out_channels)):
+            if padding_flag:
+                padding = 3 if k == 7 else 2
+            else:
+                padding = 0
+
+            self.cnn.append(
+                nn.Conv2d(
+                    in_channels=c_in, out_channels=c_out, kernel_size=k, padding=padding
+                )
+            )
+            size_out = math.floor((size_out + 2 * padding - 1 * (k - 1) - 1) / 1 + 1)
+            if i % maxpool_freq == 0 and i > 0:
+                self.cnn.append(
+                    nn.MaxPool2d(kernel_size=p_kernel[0], stride=p_stride[0], padding=0)
+                )
+                size_out = math.floor(
+                    (size_out + 2 * 0 - 1 * (p_kernel[0] - 1) - 1) / p_stride[0] + 1
+                )
+                nn.Dropout(0.5)
+        self.cnn.append(nn.Dropout(0.5))
+
+        if fc_size == 4:
+            self.fc1 = nn.Linear(size_out * size_out * out_channels[-1], 128)
+            self.fc2 = nn.Linear(128, 64)
+            self.fc3 = nn.Linear(64, 16)
+            self.fc4 = nn.Linear(16, output_size)
+        elif fc_size == 3:
+            self.fc1 = nn.Linear(size_out * size_out * out_channels[-1], 64)
+            self.fc2 = nn.Linear(64, 16)
+            self.fc3 = nn.Linear(16, output_size)
+
+        self.activation = F.relu
+        self.fc_size = fc_size
+        print("layer size: {}".format(output_size))
+        print("output size: {}".format(size_out))
+
+    def forward(self, inp):
+        out = inp
+        for l in self.cnn:
+            if isinstance(l, nn.Conv2d):
+                out = self.activation(l(out))
+            else:
+                out = l(out)
+
+        out = torch.flatten(out, 1)
+        out = self.activation(self.fc1(out))
+        out = self.activation(self.fc2(out))
+        m = torch.nn.LogSoftmax(dim=1)
+
+        if self.fc_size == 4:
+            out = self.activation(self.fc3(out))
+            out = m(self.fc4(out))
+        elif self.fc_size == 3:
+            out = m(self.fc3(out))
+
+        return out
